97f3bfdb1833b258a138be9344089f711230f872
NadineBaldi <81830398+NadineBaldi@users.noreply.github.com>
2021-08-12 12:39:08 -0300

    Delete src directory

--- src/TP_DAO/BoletoDAO.java
+++
@@ -1,9 +0,0 @@
-package TP_DAO;
-
-import Objetos.Boleto;
-
-public interface BoletoDAO {
-	
-	public void registrarVenta(Boleto b);
-
-}
--- src/TP_DAO/GrafoDAO.java
+++
@@ -1,9 +0,0 @@
-package TP_DAO;
-
-import java.sql.ResultSet;
-
-public interface GrafoDAO {
-	
-	public ResultSet mostrarTodosLosDatos(String tablaConsulta);
-
-}
